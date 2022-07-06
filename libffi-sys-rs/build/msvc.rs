ab2d5fe731266c57ceff0cc2ebdf11f072843837
Aliaksei Syrel <alex.syrel@gmail.com>
2022-07-06 11:16:58 +0430

    support aarch64 build under msvc

--- libffi-sys-rs/build/msvc.rs
+++ libffi-sys-rs/build/msvc.rs
@@ -1,6 +1,12 @@
 use crate::common::*;
 
-const INCLUDE_DIRS: &[&str] = &["libffi", "libffi/include", "include/msvc", "libffi/src/x86"];
+const INCLUDE_DIRS: &[&str] = &["libffi", "libffi/include", "include/msvc"];
+
+const INCLUDE_DIRS_AARCH64: &[&str] = &[
+    "libffi/src/aarch64",
+    "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.19041.0\\shared",
+];
+const INCLUDE_DIRS_X86: &[&str] = &["libffi/src/x86"];
 
 const BUILD_FILES: &[&str] = &[
     "tramp.c",
@@ -8,22 +14,40 @@ const BUILD_FILES: &[&str] = &[
     "prep_cif.c",
     "raw_api.c",
     "types.c",
-    "x86/ffi.c",
 ];
 
-const BUILD_FILES_X64: &[&str] = &["x86/ffiw64.c"];
+const BUILD_FILES_AARCH64: &[&str] = &["aarch64/ffi.c"];
+const BUILD_FILES_X86: &[&str] = &["x86/ffi.c"];
+const BUILD_FILES_X86_64: &[&str] = &["x86/ffi.c", "x86/ffiw64.c"];
 
 fn add_file(build: &mut cc::Build, file: &str) {
     build.file(format!("libffi/src/{}", file));
 }
 
 pub fn build_and_link() {
-    let target = env::var("TARGET").unwrap();
-    let is_x64 = target.contains("x86_64");
-    let asm_path = pre_process_asm(INCLUDE_DIRS, &target, is_x64);
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_includes = match target_arch.as_str() {
+        "x86_64" | "x86" => INCLUDE_DIRS_X86,
+        "aarch64" => INCLUDE_DIRS_AARCH64,
+        _ => panic!("Unsupported arch: {}", &target_arch),
+    };
+
+    let mut all_includes = vec![];
+    all_includes.extend(INCLUDE_DIRS);
+    all_includes.extend(target_includes);
+
+    let asm_path = pre_process_asm(all_includes.as_slice(), target_arch.as_str());
     let mut build = cc::Build::new();
 
-    for inc in INCLUDE_DIRS {
+    for inc in all_includes {
+        build.include(inc);
+    }
+
+    for inc in match target_arch.as_str() {
+        "x86_64" | "x86" => INCLUDE_DIRS_X86,
+        "aarch64" => INCLUDE_DIRS_AARCH64,
+        _ => panic!("Unsupported arch: {}", &target_arch),
+    } {
         build.include(inc);
     }
 
@@ -31,10 +55,13 @@ pub fn build_and_link() {
         add_file(&mut build, file);
     }
 
-    if is_x64 {
-        for file in BUILD_FILES_X64 {
-            add_file(&mut build, file);
-        }
+    for file in match target_arch.as_str() {
+        "x86_64" => BUILD_FILES_X86_64,
+        "x86" => BUILD_FILES_X86,
+        "aarch64" => BUILD_FILES_AARCH64,
+        _ => panic!("Unsupported arch: {}", &target_arch),
+    } {
+        add_file(&mut build, file);
     }
 
     build
@@ -54,16 +81,38 @@ pub fn probe_and_link() {
     build_and_link();
 }
 
-pub fn pre_process_asm(include_dirs: &[&str], target: &str, is_x64: bool) -> String {
-    let file_name = if is_x64 { "win64_intel" } else { "sysv_intel" };
+pub fn pre_process_asm(include_dirs: &[&str], target_arch: &str) -> String {
+    let folder_name = match target_arch {
+        "x86_64" => "x86",
+        "x86" => "x86",
+        "aarch64" => "aarch64",
+        _ => panic!("Unsupported arch: {}", target_arch),
+    };
 
-    let mut cmd = cc::windows_registry::find(&target, "cl.exe").expect("Could not locate cl.exe");
-    cmd.env("INCLUDE", include_dirs.join(";"));
+    let file_name = match target_arch {
+        "x86_64" => "win64_intel",
+        "x86" => "sysv_intel",
+        "aarch64" => "win64_armasm",
+        _ => panic!("Unsupported arch: {}", target_arch),
+    };
+
+    let mut cmd = cc::windows_registry::find(&env::var("TARGET").unwrap(), "cl.exe")
+        .expect("Could not locate cl.exe");
+
+    let build = cc::Build::new();
+    for (key, value) in build.get_compiler().env() {
+        if key.to_string_lossy().to_string() == "INCLUDE".to_string() {
+            cmd.env(
+                "INCLUDE",
+                format!("{};{}", value.to_string_lossy(), include_dirs.join(";")),
+            );
+        }
+    }
 
     cmd.arg("/EP");
-    cmd.arg(format!("libffi/src/x86/{}.S", file_name));
+    cmd.arg(format!("libffi/src/{}/{}.S", folder_name, file_name));
 
-    let out_path = format!("libffi/src/x86/{}.asm", file_name);
+    let out_path = format!("libffi/src/{}/{}.asm", folder_name, file_name);
     let asm_file = fs::File::create(&out_path).expect("Could not create output file");
 
     cmd.stdout(asm_file);
